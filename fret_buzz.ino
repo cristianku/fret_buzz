6c149ae3f7d27486c7ad3eea0e0ac8e81e4e506b
cristianku <c.zantedeschi@gmail.com>
2022-11-30 21:53:38 +0100

    c

--- fret_buzz.ino
+++ fret_buzz.ino
@@ -2,16 +2,15 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-const fret_buzz_pin = 5;
+// const fret_buzz_pin = 5;
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); //need this text to work
 
-pinMode(fret_buzz_pin, INPUT_PULLUP);
 
 int time_prev = 0;
 
-boolean read_fret_buzz_pin(){
-   int read =  digitalRead (fret_buzz_pin);
+boolean read_fret_buzz_pin(int x){
+   int read =  digitalRead (x);
 
    if ( read == 1){
      return false;
@@ -25,32 +24,63 @@ boolean read_fret_buzz_pin(){
 
 void setup() {
   // Serial.begin(9600);
-{
+
+  pinMode(5, INPUT_PULLUP);
+  pinMode(2,OUTPUT); //buzzer connesso al pin 2
+
   // initialize the LCD
    lcd.begin(16,2);
 
   // Turn on the blacklight and print a message.
-   lcd.backlight();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print(".");
+  // lcd.clear(); 
  
-}
 }
 
 void loop() {
-  if read_fret_buzz_pin(){
+  if (read_fret_buzz_pin(5) && time_prev == 0 ){
     time_prev = millis();
     // fret buzz on the screen will stay for a while
+    lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("FRET_BUZZ"); 
+    tone(2, 1000, 500); //suona una nota alla frequenza di 1000Hz per la durata di 500 ms
+// delay(1500); //attende 1500 ms
+// tone(2, 1000, 500);
+// delay(1500);
+// tone(2, 1000, 2000); //suona una nota alla frequenza di 1000Hz per la durata di 2 secondi
+// delay(2000); //attende 2 secondi
   }
    
-  if (millis() - time_prev > 1000){
+  if ((millis() - time_prev > 5000) && (time_prev > 0)  ){
     // reset the screen 
+    lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print(".");
+    time_prev = 0;
   } 
   delay(100);
+  // lcd.clear(); 
 
 }
 
  
+ // buzzer
+//  void setup()
+// {
+// pinMode(2,OUTPUT); //buzzer connesso al pin 2
+// }
+// void loop()
+// {
+// tone(2, 1000, 500); //suona una nota alla frequenza di 1000Hz per la durata di 500 ms
+// delay(1500); //attende 1500 ms
+// tone(2, 1000, 500);
+// delay(1500);
+// tone(2, 1000, 2000); //suona una nota alla frequenza di 1000Hz per la durata di 2 secondi
+// delay(2000); //attende 2 secondi
+// }
+
+
   
