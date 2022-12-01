d7a0185d425d12399d54bf32866ab7707780ad40
cristianku <c.zantedeschi@gmail.com>
2022-12-01 09:55:53 +0100

    c

--- fret_buzz.ino
+++ fret_buzz.ino
@@ -1,6 +1,7 @@
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#define BUZZER_PIN 10
 
 // const fret_buzz_pin = 5;
 
@@ -21,12 +22,29 @@ boolean read_fret_buzz_pin(int x){
 }
 
 
+void buzz (){
+// Software PWM with duty cycle of 50%, and frequency
+
+    for(uint8_t i = 0; i < 100; i++) {  // 500Hz frequency
+      digitalWrite(BUZZER_PIN, HIGH);
+      delay(2);  // Delay 1ms
+      digitalWrite(BUZZER_PIN, LOW);
+      delay(1);  // Delay 1ms
+    }
+    // for(uint8_t i = 0; i < 50; i++) {  // 167Hz frequency
+    //   digitalWrite(BUZZER_PIN, HIGH);
+    //   delay(3);  // Delay 3ms
+    //   digitalWrite(BUZZER_PIN, LOW);
+    //   delay(3);  // Delay 3ms
+    // }
+
+}
 
 void setup() {
   // Serial.begin(9600);
 
   pinMode(5, INPUT_PULLUP);
-  pinMode(2,OUTPUT); //buzzer connesso al pin 2
+  pinMode(BUZZER_PIN,OUTPUT); //buzzer connesso al pin 2
 
   // initialize the LCD
    lcd.begin(16,2);
@@ -34,7 +52,7 @@ void setup() {
   // Turn on the blacklight and print a message.
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print(".");
+  lcd.print("waiting...");
   // lcd.clear(); 
  
 }
@@ -46,7 +64,8 @@ void loop() {
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("FRET_BUZZ"); 
-    tone(2, 1000, 500); //suona una nota alla frequenza di 1000Hz per la durata di 500 ms
+    buzz();
+    // tone(2, 1000, 500); //suona una nota alla frequenza di 1000Hz per la durata di 500 ms
 // delay(1500); //attende 1500 ms
 // tone(2, 1000, 500);
 // delay(1500);
@@ -54,11 +73,11 @@ void loop() {
 // delay(2000); //attende 2 secondi
   }
    
-  if ((millis() - time_prev > 5000) && (time_prev > 0)  ){
+  if ((millis() - time_prev > 2000) && (time_prev > 0)  ){
     // reset the screen 
     lcd.clear(); 
     lcd.setCursor(0,0);
-    lcd.print(".");
+    lcd.print("waiting...");
     time_prev = 0;
   } 
   delay(100);
@@ -84,3 +103,5 @@ void loop() {
 
 
   
+
+
